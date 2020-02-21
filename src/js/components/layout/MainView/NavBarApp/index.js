a0e6a632df278c38bc9ee5aac8202a2f7cf916d2
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-21 14:22:08 +0100

    user first entry corrected

--- src/js/components/layout/MainView/NavBarApp/index.js
+++ src/js/components/layout/MainView/NavBarApp/index.js
@@ -1,18 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-
 import { getUser } from "../../../../../redux/actions/user";
-
 const NavBarApp = () => {
-  const userName = useSelector(state => state.user);
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getUser());
   });
-
+  const userName = useSelector(state => state.user);
   return (
     <div className="navbar__dashboard-container">
       <div className="navbar__dashboard-content">
@@ -23,7 +18,7 @@ const NavBarApp = () => {
           </h1>
         </Link>
         <div className="avatar__container">
-          <span className="avatar__name">Name</span>
+          <span className="avatar__name">{userName}</span>
           <svg
             className="avatar__image"
             viewBox="0 0 385 385"
@@ -56,5 +51,4 @@ const NavBarApp = () => {
     </div>
   );
 };
-
 export default NavBarApp;
