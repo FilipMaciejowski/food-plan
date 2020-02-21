5932a3f54a99e3f4236eb7caf3f7b3db8dd37a35
maciejsawinski <macsawinski@gmail.com>
2020-02-21 19:23:10 +0100

    added schedule overview component

--- src/js/components/layout/MainView/Dashboard/DashboardContent/index.js
+++ src/js/components/layout/MainView/Dashboard/DashboardContent/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import WidgetsAdd from './WidgetsAdd';
-import Notifications from './Notifications';
+import React from "react";
 
+import WidgetsAdd from "./WidgetsAdd";
+import Notifications from "./Notifications";
+import ScheduleOverview from "./ScheduleOverview";
 
 const DashboardContent = () => {
   return (
     <div className="dashboard__content-top">
       <Notifications />
       <WidgetsAdd />
+      <ScheduleOverview />
     </div>
-
   );
 };
-
 
 export default DashboardContent;
