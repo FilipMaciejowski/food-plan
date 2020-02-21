71261f1f6951173560cc6368ad22141fdcbe4a5b
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-21 22:23:02 +0100

    widgets changed

--- src/js/components/layout/MainView/Dashboard/DashboardContent/index.js
+++ src/js/components/layout/MainView/Dashboard/DashboardContent/index.js
@@ -6,13 +6,15 @@ import ScheduleOverview from "./ScheduleOverview";
 
 const DashboardContent = () => {
   return (
+    <>
     <div className="dashboard__content-top">
       <Notifications />
       <WidgetsAdd />
+    </div>
       <div className="dashboard__content-bottom">
         <ScheduleOverview />
       </div>
-    </div>
+    </>
   );
 };
 
