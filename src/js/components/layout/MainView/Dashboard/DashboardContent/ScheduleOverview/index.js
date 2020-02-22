8b5ce85c30ee08b874f4755a818e0d8723b84ab7
maciejsawinski <macsawinski@gmail.com>
2020-02-22 18:16:29 +0100

    catching up with master

--- src/js/components/layout/MainView/Dashboard/DashboardContent/ScheduleOverview/index.js
+++ src/js/components/layout/MainView/Dashboard/DashboardContent/ScheduleOverview/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ScheduleOverview = () => {
-  return <div>Schedule overview</div>;
+  return (
+    <>
+      <div className="schedule__overview-title">
+        Twój plan na nr_tygodnia tydzień:
+      </div>
+      <div className="schedule__overview-content-container"></div>
+    </>
+  );
 };
 
 export default ScheduleOverview;
--- src/js/components/layout/MainView/Dashboard/DashboardContent/index.js
+++ src/js/components/layout/MainView/Dashboard/DashboardContent/index.js
@@ -6,9 +6,11 @@ import ScheduleOverview from "./ScheduleOverview";
 
 const DashboardContent = () => {
   return (
-    <div className="dashboard__content-top">
-      <Notifications />
-      <WidgetsAdd />
+    <div className="dashboard__content">
+      <div className="dashboard__content-top">
+        <Notifications />
+        <WidgetsAdd />
+      </div>
       <div className="dashboard__content-bottom">
         <ScheduleOverview />
       </div>
