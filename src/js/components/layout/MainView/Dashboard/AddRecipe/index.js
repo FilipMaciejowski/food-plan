6d93644567cd794a94cf65d6ff7b19faa0b6bcd1
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-22 17:25:13 +0100

    AddRecipe hardcore view/styling done

--- src/js/components/layout/MainView/Dashboard/AddRecipe/index.js
+++ src/js/components/layout/MainView/Dashboard/AddRecipe/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
 
+
 const AddRecipe = () => {
 
   const onSumbit=(e)=>{
     e.preventDefault();
   };
 
-  
 
   return (
-    <div className="add__recipe__modal">
-      <div className="modal__header">
+    <div className="add__recipe__container">
+      <div className="add__recipe__header">
         <h1>new recipe</h1>
-        <button classname="modal__button">
+        <button classname="add__recipe__button">
           save and close
         </button>
       </div>
-      <div className="modal__input">
+
+      <div className="add__recipe__input">
         <form onSubmit={()=>onSumbit}>
           <label>
             name
@@ -26,9 +27,21 @@ const AddRecipe = () => {
           <label>
             description
           <input type="text" value="Submit"/>
-          </label>
-
+        </label>
         </form>
+      </div>
+
+      <div className="add__recipe__content">
+        <div className="add__recipe__content-intructions">
+          <ul className="instructions-list">
+            <li className="instructions-element">Turn on the oven</li>
+          </ul>
+        </div>
+        <div className="add__recipe__content-ingridients">
+          <ul className="ingridients-list">
+            <li className="ingridients-element">Couliflower 2</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
