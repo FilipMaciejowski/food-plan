a6006faac3de57c58e6afe2cf0dc17d3356a111a
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-02-28 22:57:29 +0100

    show schedule styled

--- src/js/components/layout/MainView/Schedules/index.js
+++ src/js/components/layout/MainView/Schedules/index.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSchedules } from "../../../../../redux/actions/schedules";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlusSquare,
+  faEdit,
+  faTrashAlt
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
 
 const Schedules = () => {
   const dispatch = useDispatch();
@@ -12,23 +20,48 @@ const Schedules = () => {
   }, []);
 
   return (
-    <>
-      {schedules ? (
-        <div>
-          {schedules.map(schedule => (
-            <div>
-              <div>{schedule.id}</div>
-              <div>{schedule.name}</div>
-              <div>{schedule.description}</div>
-              <div>{schedule.weekNumber}</div>
-              {console.log(schedule.weekNumber)}
-            </div>
-          ))}
+
+     <div className="recipes__content-container">
+      <div className="recipes__content__view">
+        <div className="recipes__content__view-header">
+          <h1>list of recipes</h1>
+          <Link to="/foodplan/schedules/add" className="icon-plus">
+            <FontAwesomeIcon className="recipes-icon-plus" icon={faPlusSquare} />
+          </Link>
         </div>
-      ) : (
-        <div>Michal nie dziala </div>
-      )}
-    </>
+        <table className="recipes__content__table">
+          <tr className="recipes__content__table-header">
+            <th>id</th>
+            <th>name</th>
+            <th>description</th>
+            <th>week</th>
+
+            <th className="table__header__cell-actions">actions</th>
+          </tr>
+          {schedules.map(schedule => (
+            <tr className="recipes__content__table-cell">
+              <td>{schedule.id}</td>
+              <td>{schedule.name}</td>
+              <td>{schedule.description}</td>
+              <td>{schedule.weekNumber}</td>
+
+              <td>
+                <FontAwesomeIcon
+                  className="icon-edit"
+                  icon={faEdit}
+                  
+                />
+                <FontAwesomeIcon
+                  className="icon-delete"
+                  icon={faTrashAlt}
+                  
+                />
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
   );
 };
 
